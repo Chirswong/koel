5d0146d4b0cd3cc7ada5ca3ced933ebbb135f612
wj <969576137@qq.com>
2020-12-01 13:48:10 +0800

    helpers

--- app/Helpers.php
+++ app/Helpers.php
@@ -7,6 +7,21 @@ function album_cover_path($fileName)
     return public_path(config('koel.album_cover_dir') . $fileName);
 }
 
-function album_cover_url($fileName){
+function album_cover_url($fileName)
+{
+    return app()->setLocale();
+}
 
+function album_thumbnail_url($filename)
+{
+    return album_cover_url($filename);
+}
+
+function artist_image_path($filename)
+{
+    return public_path(config('koel.artist_image_dir') . $filename);
+}
+
+function artist_image_url($filename){
+    return;
 }
