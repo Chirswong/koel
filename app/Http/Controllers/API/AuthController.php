930d32b32fbf08c4cfd20f6036e4880657247288
wj <969576137@qq.com>
2020-12-01 15:54:45 +0800

    service

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\API;
 
 
+use App\Service\TokenManager;
 use Illuminate\Http\Request;
+use Illuminate\Hashing\HashManager;
+use App\Repositories\UserRepository;
+use Illuminate\Auth\Authenticatable;
+
 
 class AuthController extends Controller
 {
@@ -13,8 +18,16 @@ class AuthController extends Controller
 
     private $currentUser;
 
-    public function __construct()
+    public function __construct(
+        HashManager $hash,
+        TokenManager $tokenManager,
+        ?Authenticatable $currentUser,
+        UserRepository $userRepository
+    )
     {
-
+        $this->hash = $hash;
+        $this->currentUser = $currentUser;
+        $this->tokenManager = $tokenManager;
+        $this->userRepository = $userRepository;
     }
 }
