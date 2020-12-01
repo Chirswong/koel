c8df5f92f8647649c9d5867c34173a25624f3dfb
wj <969576137@qq.com>
2020-12-01 14:55:31 +0800

    rev

--- app/Application.php
+++ app/Application.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use InvalidArgumentException;
 use Illuminate\Foundation\Application as IlluminateApplication;
 
 /**
@@ -12,6 +13,31 @@ class Application extends IlluminateApplication
 {
 
     const KOEK_VERSION = 'v4.4.0';
+
+
+    /**
+     * @param string $file
+     * @param string|null $manifestFile
+     * @return string
+     */
+    public function rev(string $file, string $manifestFile = null): string
+    {
+        static $manifest = null;
+
+        $manifestFile = $manifestFile ?: public_path('mix-manifest.json');
+
+        if ($manifest === null){
+            $manifest = json_decode(file_get_contents($manifestFile),true);
+        }
+
+        if (isset($manifest[$file])){
+            return file_exists(public_path('hot'))
+                ? "http://localhost:8080{$manifest[$file]}"
+                : $this->staticUrl("{$manifest[$file]}");
+        }
+
+        throw new InvalidArgumentException("File {$file} not defined in asset manifest");
+    }
 
     /**
      * @param null $name
--- app/Helpers.php
+++ app/Helpers.php
@@ -9,7 +9,7 @@ function album_cover_path($fileName)
 
 function album_cover_url(string $fileName): string
 {
-    return app()->staticUrl(config('koel.album_cover_dir').$fileName);
+    return app()->staticUrl(config('koel.album_cover_dir') . $fileName);
 }
 
 function album_thumbnail_url($filename)
@@ -22,6 +22,7 @@ function artist_image_path($filename)
     return public_path(config('koel.artist_image_dir') . $filename);
 }
 
-function artist_image_url($filename){
-    return;
+function artist_image_url(string $filename): string
+{
+    return app()->staticUrl(config('koel.artist_image_dir') . $filename);
 }
